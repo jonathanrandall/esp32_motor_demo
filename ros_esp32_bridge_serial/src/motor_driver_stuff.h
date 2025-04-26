581882621ac62ff111d3ebc80ce524bfc6a41a27
omgupta113 <omgupta113@gmail.com>
2025-04-26 19:26:52 +0530

    Replace L298N with BTS7960 motor driver implementation

--- ros_esp32_bridge_serial/src/motor_driver_stuff.h
+++ ros_esp32_bridge_serial/src/motor_driver_stuff.h
@@ -1,67 +1,95 @@
-#ifndef MOTORS_STUFF_
-#define MOTORS_STUFF_
+#ifndef MOTORSSTUFF
+#define MOTORSSTUFF
 #include <Arduino.h>
 #include "commands.h"
-//pins
-#define LEFT_MOTOR_PWM 25
-#define RIGHT_MOTOR_PWM 27
-#define RIGHT_MOTOR_DIR 12
-#define LEFT_MOTOR_DIR 26
 
-//pwm paramters
-const int mtr_left_pwm_channel = 8;
-const int mtr_right_pwm_channel = 9;
+// BTS7960 Motor Control Pins
+// Left Motor
+#define LEFT_MOTOR_LPWM 33  // L PWM pin (reverse)
+#define LEFT_MOTOR_RPWM 32  // R PWM pin (forward)
+#define LEFT_MOTOR_EN 5     // Enable pin (optional, can be tied to HIGH)
+
+// Right Motor
+#define RIGHT_MOTOR_LPWM 27 // L PWM pin (reverse)
+#define RIGHT_MOTOR_RPWM 26 // R PWM pin (forward)
+#define RIGHT_MOTOR_EN 25   // Enable pin (optional, can be tied to HIGH)
+
+// PWM parameters
+const int mtr_left_rpwm_channel = 5;
+const int mtr_left_lpwm_channel = 6;
+const int mtr_right_rpwm_channel = 7;
+const int mtr_right_lpwm_channel = 8;
 const int lresolution = 8;
 const int freq = 4000;
 
-void robot_setup()
-{
-    // Pins for Motor Controller
-    pinMode(LEFT_MOTOR_DIR, OUTPUT);
-    pinMode(LEFT_MOTOR_PWM, OUTPUT);
-    pinMode(RIGHT_MOTOR_DIR, OUTPUT);
-    pinMode(RIGHT_MOTOR_PWM, OUTPUT);
-
+void robot_setup() {
+    // Configure motor control pins
+    pinMode(LEFT_MOTOR_LPWM, OUTPUT);
+    pinMode(LEFT_MOTOR_RPWM, OUTPUT);
+    pinMode(LEFT_MOTOR_EN, OUTPUT);
     
+    pinMode(RIGHT_MOTOR_LPWM, OUTPUT);
+    pinMode(RIGHT_MOTOR_RPWM, OUTPUT);
+    pinMode(RIGHT_MOTOR_EN, OUTPUT);
 
-    // Motor uses PWM Channel 8
-    ledcSetup(mtr_left_pwm_channel, freq, lresolution);
-    ledcAttachPin(LEFT_MOTOR_PWM, mtr_left_pwm_channel);    
-    ledcWrite(mtr_left_pwm_channel, 0);
+    // Set enable pins high
+    digitalWrite(LEFT_MOTOR_EN, HIGH);
+    digitalWrite(RIGHT_MOTOR_EN, HIGH);
 
-    ledcSetup(mtr_right_pwm_channel, freq, lresolution);
-    ledcAttachPin(RIGHT_MOTOR_PWM, mtr_right_pwm_channel);    
-    ledcWrite(mtr_right_pwm_channel, 0);
-        
+    // Configure PWM channels
+    ledcSetup(mtr_left_rpwm_channel, freq, lresolution);
+    ledcAttachPin(LEFT_MOTOR_RPWM, mtr_left_rpwm_channel);
+    ledcWrite(mtr_left_rpwm_channel, 0);
+
+    ledcSetup(mtr_left_lpwm_channel, freq, lresolution);
+    ledcAttachPin(LEFT_MOTOR_LPWM, mtr_left_lpwm_channel);
+    ledcWrite(mtr_left_lpwm_channel, 0);
+
+    ledcSetup(mtr_right_rpwm_channel, freq, lresolution);
+    ledcAttachPin(RIGHT_MOTOR_RPWM, mtr_right_rpwm_channel);
+    ledcWrite(mtr_right_rpwm_channel, 0);
+
+    ledcSetup(mtr_right_lpwm_channel, freq, lresolution);
+    ledcAttachPin(RIGHT_MOTOR_LPWM, mtr_right_lpwm_channel);
+    ledcWrite(mtr_right_lpwm_channel, 0);
 }
 
 void setMotorSpeed(int i, int spd) {
-  unsigned char reverse = 0;
+    unsigned char reverse = 0;
+    if (spd < 0) {
+        spd = -spd;
+        reverse = 1;
+    }
+    if (spd > 255) spd = 255;
 
-  if (spd < 0)
-  {
-    spd = -spd;
-    reverse = 1;
-  }
-  if (spd > 255)
-    spd = 255;
-  //note left and right motors facing in opposite directions.
-  if (i == LEFT) { 
-    if      (reverse == 0) {  digitalWrite(LEFT_MOTOR_DIR, 1); }
-    else if (reverse == 1) {  digitalWrite(LEFT_MOTOR_DIR, 0); }
-    ledcWrite(mtr_left_pwm_channel, spd);
-  }
-  else /*if (i == RIGHT) //no need for condition*/ {
-    if      (reverse == 0) {  digitalWrite(RIGHT_MOTOR_DIR, 0); }
-    else if (reverse == 1) {  digitalWrite(RIGHT_MOTOR_DIR, 1); }
-    ledcWrite(mtr_right_pwm_channel, spd);
-  }
+    if (i == LEFT) {
+        // Left motor control for BTS7960
+        if (reverse) {
+            // Reverse: LPWM active, RPWM inactive
+            ledcWrite(mtr_left_rpwm_channel, 0);
+            ledcWrite(mtr_left_lpwm_channel, spd);
+        } else {
+            // Forward: RPWM active, LPWM inactive
+            ledcWrite(mtr_left_rpwm_channel, spd);
+            ledcWrite(mtr_left_lpwm_channel, 0);
+        }
+    } else {  // RIGHT motor
+        // Right motor control for BTS7960
+        if (reverse) {
+            // Reverse: LPWM active, RPWM inactive
+            ledcWrite(mtr_right_rpwm_channel, 0);
+            ledcWrite(mtr_right_lpwm_channel, spd);
+        } else {
+            // Forward: RPWM active, LPWM inactive
+            ledcWrite(mtr_right_rpwm_channel, spd);
+            ledcWrite(mtr_right_lpwm_channel, 0);
+        }
+    }
 }
 
 void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-  setMotorSpeed(LEFT, leftSpeed);
-  setMotorSpeed(RIGHT, rightSpeed);
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
 }
-
 
 #endif
